649a4d1f063e3cd4673c40eae1d30126132a392f
tomoari <1393894753@qq.com>
2025-06-07 04:11:42 +0800

    修改了网站左上角和底部名字，改变头像

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = '南墙 Southern Wall';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
